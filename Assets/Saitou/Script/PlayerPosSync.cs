4eeef94227f3b1d880f5fa9417cd903c067af9d5
yukiSaitou <nkc.yuki.saitou@gmail.com>
2019-10-08 01:43:14 +0900

    位置同期

--- Assets/Saitou/Script/PlayerPosSync.cs
+++ Assets/Saitou/Script/PlayerPosSync.cs
@@ -24,13 +24,13 @@ namespace Saitou.Online
         // Update is called once per frame
         void Update()
         {
-            if ((int)state._PlayerList == manager.PlayerID)
-            {
-                //現在の移動速度
-                var velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-                //移動速度を指定
-                _photonTransform.SetSynchronizedValues(speed: velocity, turnSpeed: 0);
-            }
+            //if ((int)state._PlayerList == manager.PlayerID)
+            //{
+            //    //現在の移動速度
+            //    var velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+            //    //移動速度を指定
+            //    _photonTransform.SetSynchronizedValues(speed: velocity, turnSpeed: 0);
+            //}
         }
     }
 }

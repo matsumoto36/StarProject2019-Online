ee3637a03f2a5a6ccc480f7e90ec2c35e1aae540
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-10-08 14:43:04 +0900

    ロビー作成

---
+++ Assets/Matsumoto/Scripts/Stages/LobbyButtonController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+
+public class LobbyButtonController : MonoBehaviour {
+
+	public Button[] Buttons;
+	public RectTransform Cursor;
+
+	public event UnityAction OnCreateRoomButtonClick;
+	public event UnityAction OnInRoomButtonClick;
+	public event UnityAction OnBackButtonClick;
+
+	private int _selectionButton = 0;
+	private float _controllerDead = 0.3f;
+	private bool _canMoveCursor = true;
+
+	public bool IsActive {
+		get; set;
+	} = true;
+
+	private void Awake() {
+
+		Buttons[0].onClick.AddListener(() => {
+			if(!IsActive) return;
+			OnCreateRoomButtonClick?.Invoke();
+			});
+		Buttons[1].onClick.AddListener(() => {
+			if(!IsActive) return;
+			OnInRoomButtonClick?.Invoke();
+		});
+		Buttons[2].onClick.AddListener(() => {
+			if(!IsActive) return;
+			OnBackButtonClick?.Invoke();
+		});
+
+	}
+
+	private void Update() {
+		if(!IsActive) return;
+
+		if(Input.GetButtonDown("Attack")) {
+			switch(_selectionButton) {
+				case 0: OnCreateRoomButtonClick?.Invoke(); break;
+				case 1: OnInRoomButtonClick?.Invoke(); break;
+				case 2: OnBackButtonClick?.Invoke(); break;
+			}
+		}
+
+		var input = Input.GetAxisRaw("Horizontal");
+
+		if(Mathf.Abs(input) < _controllerDead) {
+			_canMoveCursor = true;
+			return;
+		}
+
+		if(!_canMoveCursor) return;
+
+		_canMoveCursor = false;
+
+		_selectionButton = (int)Mathf.Clamp(_selectionButton + Mathf.Sign(input), 0, 2);
+		Cursor.position = Buttons[_selectionButton].transform.position;
+
+	}
+}
---
+++ Assets/Matsumoto/Scripts/Stages/LobbyStageController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+using UnityEngine.SceneManagement;
+using Matsumoto.Gimmick;
+using Matsumoto.Audio;
+using Matsumoto.Character;
+using Matsumoto;
+
+public class LobbyStageController : MonoBehaviour, IStageController {
+
+	public event Action<IStageController> OnGameStart;
+	public event Action<IStageController> OnGameClear;
+	public event Action<IStageController> OnGameOver;
+
+	public LobbyButtonController ButtonController;
+
+	private List<GimmickChip> _gimmicks = new List<GimmickChip>();
+
+	public bool CanPause {
+		get; set;
+	} = false;
+
+	public GameState State {
+		get; private set;
+	} = GameState.StartUp;
+
+	private void Awake() {
+
+		// ギミック
+		_gimmicks = FindObjectsOfType<GimmickChip>().ToList();
+		foreach(var item in _gimmicks) {
+			item.Controller = this;
+			item.GimmickStart();
+		}
+
+		ButtonController.OnBackButtonClick += () => {
+			AudioManager.FadeOut(1.0f);
+			SceneChanger.Instance.MoveScene("StageSelect", 1.0f, 1.0f, SceneChangeType.BlackFade);
+		};
+
+		// プレイヤーがゴールしたらゲームクリア
+		FindObjectOfType<PlayerGoalChip>().OnGoalPlayer += (_) => {
+			BackToSelectScene();
+		};
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		// BGMを鳴らす
+		AudioManager.FadeIn(1.0f, "vigilante");
+
+		GameStart();
+	}
+
+	public void GameStart() {
+
+		Debug.Log("GameStart!");
+
+		State = GameState.Playing;
+
+		//OnGameStart?.Invoke(this);
+
+		//CanPause = true;
+	}
+
+	public void BackToSelectScene() {
+
+		Debug.Log("return");
+
+		State = GameState.GameClear;
+
+		OnGameClear?.Invoke(this);
+		CanPause = false;
+
+		AudioManager.FadeOut(1.0f);
+		SceneChanger.Instance.MoveScene("StageSelect", 1.0f, 1.0f, SceneChangeType.BlackFade);
+	}
+}
